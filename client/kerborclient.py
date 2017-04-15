c5a762a2aba306cbda107c2fcf17169be1ef892d
Kiryl Bacharou <bacharou.kiryl@yandex.by>
2017-04-15 19:54:20 +0300

    Check emitter for errors

--- client/kerborclient.py
+++ client/kerborclient.py
@@ -1,4 +1,5 @@
 from shared import messages
+import hmac
 
 
 class KerborClient(object):
@@ -10,7 +11,7 @@ class KerborClient(object):
     def __init__(self, _username, _password):
         self.user_name = _username
         self.user_password = _password
-        self.user_secret = 'SECRETsecretSECRETsecret12345678'
+        self.user_secret = hmac.new(bytes(_password), bytes(_username)).hexdigest()
 
     def set_emitter(self, _emitter):
         self.emitter = _emitter
@@ -20,15 +21,21 @@ class KerborClient(object):
             raise TypeError("emitter should be instance of Emitter")
 
         auth = messages.AuthenticateMeMessage(self.user_name, tgs)
-        auth_response = self.emitter.emit(auth)
+        check, auth_response = self.emitter.emit(auth)
+        if not check:
+            raise ValueError("Something went wrong")
 
         tgs_sess_key = auth_response.get_sess_key(self.user_secret)
         id_message = messages.IDMessage(self.user_name)
         ticket_request = messages.TicketRequestMessage(service, id_message, tgs_sess_key, auth_response.tgt)
-        ticket_response = self.emitter.emit(ticket_request)
+        check, ticket_response = self.emitter.emit(ticket_request)
+        if not check:
+            raise ValueError("Something went wrong")
 
         service_sess_key = ticket_response.get_c_s_key(tgs_sess_key)
         service_request = messages.ServiceRequestMessage(id_message, service_sess_key, ticket_response.grant)
-        service_response = self.emitter.emit(service_request)
+        check, service_response = self.emitter.emit(service_request)
+        if not check:
+            raise ValueError("Something went wrong")
 
         return service_response.verify_time(service_sess_key, id_message.timestamp)
